ee789681b024966435a3d4c29d28d3516061525b
David Pascual <paskymail@gmail.com>
2020-02-22 22:17:48 +0100

    test3

---
+++ Entrenar.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras import optimizers
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
+from tensorflow.python.keras.layers import  Convolution2D, MaxPooling2D
+from tensorflow.python.keras import backend as K
+
+K.clear_session
+
---
+++ Predecir.py
@@ -0,0 +1,3 @@
+import numpy as np
+a = 2+2
+print(a) 
