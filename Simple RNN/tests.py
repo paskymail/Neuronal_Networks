87126d50c8a8bd54857f263616889c432f69f0bc
David Pascual <paskymail@gmail.com>
2020-04-16 23:05:34 +0200

    clasification categories

--- Simple RNN/tests.py
+++ Simple RNN/tests.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import json
+import os
 
-test = "[1,2,3]"
-prueba = eval (test)
-print(prueba[1])
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, 'class_to_action.json')) as json_file:
+    CtA = json.load(json_file)
+
+def class_to_action (CtA, class_int):
+    action = CtA[class_int]
+    return action
+
+prueba = class_to_action(CtA,"0")
+
+print(prueba[0]+ prueba[1]+1)
