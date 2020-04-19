0a3b32a33d000a2473d75b80240a9b0c16a19a0f
David Pascual <paskymail@gmail.com>
2020-04-19 21:25:43 +0200

    Categorical multi step in RNN TS V3

--- Simple RNN/tests.py
+++ Simple RNN/tests.py
@@ -1,16 +1,5 @@
 import pandas as pd
-import json
-import os
-
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-with open(os.path.join(__location__, 'class_to_action.json')) as json_file:
-    CtA = json.load(json_file)
-
-def class_to_action (CtA, class_int):
-    action = CtA[class_int]
-    return action
-
-prueba = class_to_action(CtA,"0")
-
-print(prueba[0]+ prueba[1]+1)
+import numpy as np
+rng = np.random.RandomState(0)
+X = rng.random_sample((10, 3))
+print(X)
