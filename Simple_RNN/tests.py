44e2e55519a884bfdca61c6fd74eac90a23f00d5
David Pascual <paskymail@gmail.com>
2020-06-15 22:57:41 +0200

    organizatoin

--- Simple_RNN/tests.py
+++ Simple_RNN/tests.py
@@ -3,3 +3,7 @@ import numpy as np
 rng = np.random.RandomState(0)
 X = rng.random_sample((10, 3))
 print(X)
+
+from ipynb.fs.full.Process_Training_Data import class_to_action
+
+print(class_to_action(1))
