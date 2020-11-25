f7cf78a6735a502835e749cb8c2bfe96458bc624
David Pascual <paskymail@gmail.com>
2020-11-25 23:04:57 +0100

    archiving

--- Predecir.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a = np.ones((3,4))
-print(a)
--- Simple_RNN/tests.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-rng = np.random.RandomState(0)
-X = rng.random_sample((10, 3))
-print(X)
-
-from ipynb.fs.full.Process_Training_Data import class_to_action
-
-print(class_to_action(1))
